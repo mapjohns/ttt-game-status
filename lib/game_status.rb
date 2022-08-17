6c0c8eedd918fbaed749f0989027a4849da5bb39
Matthew <mattj20@gmail.com>
2022-08-17 16:48:31 -0700

    commit tree 9994104c430e3db09377c9557fb1ebb9059e4460

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+
+def won?(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |combo|
+    if board[combo[0]] == board[combo[1]] && board[combo[0]] == board[combo[2]] && position_taken?(board, combo[0]) == true
+      winner = combo
+    end
+  end
+  return winner.nil? ? false : winner
+end
+
+def full?(board)
+  total = 0
+  board.each do |spot|
+    if spot == "X" || spot == "O"
+      total += 1
+    end
+  end
+  return total == 9 ? true : false
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true
+    return true
+  elsif won?(board) == false && full?(board) == false || won?(board) == true
+    return false
+  end
+end
+
+def over?(board)
+  if draw?(board) == true || (won?(board) != false && full?(board) == true) || (won?(board) != false && full?(board) == false)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) != false
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
